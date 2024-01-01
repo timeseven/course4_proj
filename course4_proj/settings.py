d480739115c29d1657c74d3a9ed5276443cc60e8
Even Qian <evenwin88@hotmail.com>
2024-01-01 04:58:48 +0000

    Finish github library

--- course4_proj/settings.py
+++ course4_proj/settings.py
@@ -48,7 +48,8 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
-        'movies'
+        'movies',
+        'gh'
     ]
 
     MIDDLEWARE = [
